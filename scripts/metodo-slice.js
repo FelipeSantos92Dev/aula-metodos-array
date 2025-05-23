c311f7385b00816da4aae7376643e891b1d92b24
Felipe Santos <dev.felipesantos@gmail.com>
2025-05-23 11:09:20 -0300

    feat: usa o método slice e altera o arquivo README.md

---
+++ scripts/metodo-slice.js
@@ -0,0 +1,12 @@
+let playlist = [
+  "Funk", // 0
+  "Hip-Hop", // 1
+  "Pop", // 2
+  "Rock", // 3
+  "Jazz", // 4
+  "Reggae", // 5
+  "Classical", // 6
+  "Indie", // 7
+];
+
+console.log("Parte da Playlist (índices 2 a 5): ", playlist.slice(2, 6));
