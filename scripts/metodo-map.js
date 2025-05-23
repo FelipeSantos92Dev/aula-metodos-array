b9f790172bd3771f3c6b870969dd3b7d1d74abe3
Felipe Santos <dev.felipesantos@gmail.com>
2025-05-23 11:53:02 -0300

    feat: usa o método map e altera o arquivo README.md

---
+++ scripts/metodo-map.js
@@ -0,0 +1,9 @@
+let playlist = [
+  "Funk", // 0
+  "Hip-Hop", // 1
+  "Pop", // 2
+];
+
+let playlistMaiusculo = playlist.map((musica) => musica.toUpperCase());
+
+console.log("Playlist em maiúsculas: ", playlistMaiusculo);
