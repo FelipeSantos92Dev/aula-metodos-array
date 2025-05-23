e7ff0e5cb65c7f5509a3c8bf2db258525fcbecde
Felipe Santos <dev.felipesantos@gmail.com>
2025-05-23 11:37:36 -0300

    feat: usa o método concat e altera o arquivo README.md

---
+++ scripts/metodo-concat.js
@@ -0,0 +1,12 @@
+let playlist1 = [
+  "Funk", // 0
+  "Hip-Hop", // 1
+  "Pop", // 2
+];
+
+let playlist2 = [
+  "Rock", // 3
+  "Jazz", // 4
+];
+
+console.log("Playlist combinada: ", playlist1.concat(playlist2));
